f1265c02e59f6aec9537e93d82422eaf03c73603
SeanDecker1 <43579108+SeanDecker1@users.noreply.github.com>
2019-04-29 22:04:30 -0400

    Added final submission files

---
+++ Milestone5Submission/README.md
@@ -0,0 +1,9 @@
+## How to Run
+To run the program, simply double click on the EdgeConvertProgram.jar file.
+This will launch the program's interface.
+A file titled "Courses.edg" was also included as an example/test file.
+To open the test file, click "File", then "Open Edge File", and navigate your file directory to open the file. 
+For any other help/reference, please look through the "Help" menu in the program.
+# Also included:
+The source files for the program have been included as well. Specifically the .java files.
+To recieve any other source material that may not be included, please contact the developers.
---
+++ Milestone5Submission/src/EdgeConvertCreateDDL.java
@@ -0,0 +1,66 @@
+public abstract class EdgeConvertCreateDDL {
+   static String[] products = {"MySQL"};
+   protected EdgeTable[] tables; //master copy of EdgeTable objects
+   protected EdgeField[] fields; //master copy of EdgeField objects
+   int[] numBoundTables;
+   int maxBound;
+   protected StringBuffer sb;
+   protected int selected;
+   
+   public EdgeConvertCreateDDL(EdgeTable[] tables, EdgeField[] fields) {
+      this.tables = tables;
+      this.fields = fields;
+      initialize();
+   } //EdgeConvertCreateDDL(EdgeTable[], EdgeField[])
+   
+   public EdgeConvertCreateDDL() { //default constructor with empty arg list for to allow output dir to be set before there are table and field objects
+      
+   } //EdgeConvertCreateDDL()
+
+   private void initialize() {
+      numBoundTables = new int[tables.length];
+      maxBound = 0;
+      sb = new StringBuffer();
+
+      for (int i = 0; i < tables.length; i++) { //step through list of tables
+         int numBound = 0; //initialize counter for number of bound tables
+         int[] relatedFields = tables[i].getRelatedFieldsArray();
+         for (int relatedField : relatedFields) { //step through related fields list
+            if (relatedField != 0) {
+               numBound++; //count the number of non-zero related fields
+            }
+         }
+         numBoundTables[i] = numBound;
+         if (numBound > maxBound) {
+            maxBound = numBound;
+         }
+      }
+   }
+   
+   EdgeTable getTable(int numFigure) {
+      for (EdgeTable table : tables) {
+         if (numFigure == table.getNumFigure()) {
+            return table;
+         }
+      }
+      return null;
+   }
+   
+   EdgeField getField(int numFigure) {
+      for (EdgeField field : fields) {
+         if (numFigure == field.getNumFigure()) {
+            return field;
+         }
+      }
+      return null;
+   }
+
+   public abstract String getDatabaseName();
+
+   public abstract String getProductName();
+   
+   public abstract String getSQLString();
+   
+   public abstract void createDDL();
+   
+}//EdgeConvertCreateDDL
